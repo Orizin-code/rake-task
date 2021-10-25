8afc4f0bff9457af5950f86ec637122d59893013
Orizin <nlgporizin10@gmail.com>
2021-10-25 09:41:27 +0900

    ヘルパーメソッドの作成

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,12 @@
 module MoviesHelper
+  def embed_youtube(_url)
+    tag.iframe(
+      width: 710,
+      height: 399,
+      src: _url,
+      frameborder: 0,
+      allow: 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture',
+      allowfullscreen: true
+    )
+  end
 end
